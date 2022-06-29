48ee65659d2b89480e5468b7041e4aea2608ad58
Joseph Balsavich <jbalsavich@gmail.com>
2022-06-28 23:43:39 -0500

    fixing /dist?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/solid-app-1/',
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
